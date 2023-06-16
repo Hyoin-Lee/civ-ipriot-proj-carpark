16d8ac98acadad073138b04444fe3ca38503d372
Scott <winroof1988@gmail.com>
2023-06-16 22:33:52 +0800

    Renamed Folder

---
+++ smartpark/mqtt_display.py
@@ -0,0 +1,115 @@
+"""The following code is used to provide an alternative to students who do not have a Raspberry Pi.
+If you have a Raspberry Pi, or a SenseHAT emulator under Debian, you do not need to use this code.
+
+You need to split the classes here into two files, one for the CarParkDisplay and one for the CarDetector.
+Attend to the TODOs in each class to complete the implementation."""
+import random
+import tkinter as tk
+from typing import Iterable
+import paho.mqtt.client as paho
+import time
+import config_parser
+
+# ------------------------------------------------------------------------------------#
+# You don't need to understand how to implement this class, just how to use it.       #
+# ------------------------------------------------------------------------------------#
+# TODO: got to the main section of this script **first** and run the CarParkDisplay.  #
+
+
+class WindowedDisplay:
+    """Displays values for a given set of fields as a simple GUI window. Use .show() to display the window; use .update() to update the values displayed.
+    """
+
+
+    DISPLAY_INIT = '– – –'
+    SEP = ':'  # field name separator
+
+    def __init__(self, title: str, display_fields: Iterable[str]):
+        """Creates a Windowed (tkinter) display to replace sense_hat display. To show the display (blocking) call .show() on the returned object.
+
+        Parameters
+        ----------
+        title : str
+            The title of the window (usually the name of your carpark from the config)
+        display_fields : Iterable
+            An iterable (usually a list) of field names for the UI. Updates to values must be presented in a dictionary with these values as keys.
+        """
+        self.window = tk.Tk()
+        self.window.title(f'{title}: Parking')
+        self.window.geometry('800x400')
+        self.window.resizable(False, False)
+        self.display_fields = display_fields
+
+        self.gui_elements = {}
+        for i, field in enumerate(self.display_fields):
+
+            # create the elements
+            self.gui_elements[f'lbl_field_{i}'] = tk.Label(
+                self.window, text=field+self.SEP, font=('Arial', 50))
+            self.gui_elements[f'lbl_value_{i}'] = tk.Label(
+                self.window, text=self.DISPLAY_INIT, font=('Arial', 50))
+
+            # position the elements
+            self.gui_elements[f'lbl_field_{i}'].grid(
+                row=i, column=0, sticky=tk.E, padx=5, pady=5)
+            self.gui_elements[f'lbl_value_{i}'].grid(
+                row=i, column=2, sticky=tk.W, padx=10)
+
+    def show(self):
+        """Display the GUI. Blocking call."""
+        self.window.mainloop()
+
+    def update(self, updated_values: dict):
+        """Update the values displayed in the GUI. Expects a dictionary with keys matching the field names passed to the constructor."""
+        for field in self.gui_elements:
+            if field.startswith('lbl_field'):
+                field_value = field.replace('field', 'value')
+                self.gui_elements[field_value].configure(
+                    text=updated_values[self.gui_elements[field].cget('text').rstrip(self.SEP)])
+        self.window.update()
+
+# -----------------------------------------#
+# TODO: STUDENT IMPLEMENTATION STARTS HERE #
+# -----------------------------------------#
+
+
+class CarParkDisplay:
+    """Provides a simple display of the car park status. This is a skeleton only. The class is designed to be customizable without requiring and understanding of tkinter or threading."""
+    # determines what fields appear in the UI
+    fields = ['Available bays', 'Temperature', 'At']
+
+    def __init__(self,config):
+        
+        self.window = WindowedDisplay(
+            config["location"], CarParkDisplay.fields)
+        
+        #updater = threading.Thread(target=self.check_updates)
+        #updater.daemon = True
+        #updater.start()
+        
+        # add mqtt code here
+        self.client = paho.Client()
+        self.client.on_message = self.on_message   #callback function
+        self.received=False
+        self.client.connect(config["broker_host"], config["broker_port"])
+        self.client.subscribe("lot/display")
+        self.client.loop_start() 
+
+        self.window.show()
+    
+    def on_message(self,client, userdata, msg):
+        print(f'Received {msg.payload.decode()}')
+        message = msg.payload.decode()
+        message.split("@")
+        fields = ['Available bays', 'Temperature', 'At']
+        field_values = dict(zip(fields, message.split("@")))
+        self.window.update(field_values)
+            
+
+
+if __name__ == '__main__':
+    # TODO: Run each of these classes in a separate terminal. You should see the CarParkDisplay update when you click the buttons in the CarDetector.
+    # These classes are not designed to be used in the same module - they are both blocking. If you uncomment one, comment-out the other.
+
+    config = config_parser.parse_config("config.toml") 
+    CarParkDisplay(config)
